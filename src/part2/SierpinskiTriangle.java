796c918298bdbe01af882daea957f2d9f93624b1
Sam Strauss <strausssa@s.dcsdk12.org>
2016-03-30 10:21:43 -0600

    ST1

--- src/part2/SierpinskiTriangle.java
+++ src/part2/SierpinskiTriangle.java
@@ -6,9 +6,9 @@ import draw.StdDraw;
 
 public class SierpinskiTriangle { 
    
-	private Point2D.Double top, right, left;
 	private Color background = new Color(34, 129, 34);
 	private Color foreground = StdDraw.YELLOW;
+	Point2D.Double top, right, left;
 	
 	public SierpinskiTriangle() {
 		StdDraw.setXscale(0, 150); 
@@ -21,9 +21,19 @@ public class SierpinskiTriangle {
 		StdDraw.clear(StdDraw.GRAY); 
 	}
    
-	public void sierpinski(Point2D.Double a, Point2D.Double b, Point2D.Double c, int n) {
-	   // TODO: implement
-    }
+	public void sierpinski(Point2D.Double top, Point2D.Double right, Point2D.Double left, int n) {
+		if (n <= 0)
+			return;
+
+		Point2D.Double lr = new Point2D.Double((right.x + left.x) / 2.0, right.y);
+		Point2D.Double tr = new Point2D.Double((top.x + right.x) / 2.0, (top.y + right.y) / 2.0);
+		Point2D.Double tl = new Point2D.Double((top.x + left.x) / 2.0, (top.y + left.y) / 2.0);
+		drawTriangle(tl, tr, lr,  Color.BLACK );
+		sierpinski(top, tr, tl, n - 1);
+		sierpinski(tr, right, lr, n - 1);
+		sierpinski(tl, lr, left, n - 1);
+
+	}
    
 	public void drawTriangle(Point2D.Double a, Point2D.Double b, Point2D.Double c, Color color) {
 	   double[] x = {a.x, b.x, c.x};
@@ -33,13 +43,12 @@ public class SierpinskiTriangle {
 	}
 	
 	public void draw(int n) {
-		sierpinski(top, right, left, n);
+		drawTriangle(top, right, left,  Color.WHITE);
+		sierpinski(this.top, this.right, this.left, n-1);
 	}
-	
-	
 	
 	public static void main(String[] args) { 
 		SierpinskiTriangle triangle = new SierpinskiTriangle();
-		triangle.draw(3);
+		triangle.draw(2);
 	}
 }
